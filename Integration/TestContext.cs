49a3ef3a559da7ff4ed6528157402b2f5244129a
Chayan Adhikari <chayan.adhikari@ivycomptech.com>
2021-07-14 02:50:20 +0530

    Adding integration tests and also adding new docker compose config for integration step

---
+++ Integration/TestContext.cs
@@ -0,0 +1,41 @@
+﻿using AccountOwnerServer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration
+{
+    public class TestContext : IDisposable
+    {
+        private TestServer _server;
+        public HttpClient Client { get; private set; }
+
+        public TestContext()
+        {
+            SetUpClient();
+        }
+
+        private void SetUpClient()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseConfiguration(new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build())
+                .UseStartup<Startup>());
+
+            Client = _server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            _server?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
